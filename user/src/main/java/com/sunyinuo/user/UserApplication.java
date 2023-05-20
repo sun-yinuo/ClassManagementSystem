81bdaf5f798e4cb85fb046597da49561aad18f2c
sun-yinuo <2192519304@qq.com>
2023-05-20 22:26:52 +0800

    user service init

---
+++ user/src/main/java/com/sunyinuo/user/UserApplication.java
@@ -0,0 +1,16 @@
+package com.sunyinuo.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author martin
+ */
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+}
