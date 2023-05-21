0ad7ff80941f6c3768c14657085a9d94435f9db2
sun-yinuo <2192519304@qq.com>
2023-05-21 14:05:24 +0800

    module-liu-calculation-exercise service init

---
+++ module-liu-calculation-exercise/src/main/java/com/sunyinuo/moduleliucalculationexercise/ModuleLiuCalculationExerciseApplication.java
@@ -0,0 +1,16 @@
+package com.sunyinuo.moduleliucalculationexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author martin
+ */
+@SpringBootApplication
+public class ModuleLiuCalculationExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleLiuCalculationExerciseApplication.class, args);
+    }
+
+}
---
+++ module-liu-calculation-exercise/src/test/java/com/sunyinuo/moduleliucalculationexercise/ModuleLiuCalculationExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyinuo.moduleliucalculationexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleLiuCalculationExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
